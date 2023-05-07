6b0061560d673736e1cf66b901f436387071c32d
pandeyabhay <pandeyabhay796@gmail.com>
2023-05-07 19:48:58 +0530

    first commit

---
+++ src/firebaseconfig.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+
+const firebaseconfig = {
+    apiKey: "AIzaSyBKHAp_hXcQt4kWHEZ2mz76_nl3-S3g9-o",
+  authDomain: "blinken-35039.firebaseapp.com",
+  projectId: "blinken-35039",
+  storageBucket: "blinken-35039.appspot.com",
+  messagingSenderId: "722532261256",
+  appId: "1:722532261256:web:aacd66671a54555a0dee3d"
+};
+
+firebase.initializeApp(firebaseconfig);
+
+export default firebaseconfig;
